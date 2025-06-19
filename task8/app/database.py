071dd22e4a9aae33bc901b4067fa1c0d4a589920
Alisher Khan <abasalisher12@gmail.com>
2025-06-19 12:17:03 +0500

    task8

---
+++ task8/app/database.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from sqlmodel import SQLModel
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
